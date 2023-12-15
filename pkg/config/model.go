eab25c679011aaecd663fcbcebcea36111f15977
İsmail Emre Sırmalı <lynicis@protonmail.com>
2023-12-15 14:12:42 +0300

    refactor: update env variable names

--- pkg/config/model.go
+++ pkg/config/model.go
@@ -14,11 +14,11 @@ const (
 const (
 	EnvironmentVariableNotDefined = "%s variable is not defined"
 
-	EnvironmentVariableGetUserByIdFunctionName                   = "USER_API_GET_USER_BY_ID_FUNCTION_NAME"                     // #nosec G101
-	EnvironmentVariableRegisterFunctionName                      = "USER_API_REGISTER_FUNCTION_NAME"                           // #nosec G101
-	EnvironmentVariableLoginFunctionName                         = "USER_API_LOGIN_FUNCTION_NAME"                              // #nosec G101
-	EnvironmentVariableGetAccessTokenViaRefreshTokenFunctionName = "USER_API_GET_ACCESS_TOKEN_VIA_REFRESH_TOKEN_FUNCTION_NAME" // #nosec G101
-	EnvironmentVariableUpdateUserByIdFunctionName                = "USER_API_UPDATE_USER_BY_ID_FUNCTION_NAME"                  // #nosec G101
+	EnvironmentVariableGetUserByIdFunctionName                   = "USER_SERVICE_GET_USER_BY_ID_FUNCTION_NAME"                     // #nosec G101
+	EnvironmentVariableRegisterFunctionName                      = "USER_SERVICE_REGISTER_FUNCTION_NAME"                           // #nosec G101
+	EnvironmentVariableLoginFunctionName                         = "USER_SERVICE_LOGIN_FUNCTION_NAME"                              // #nosec G101
+	EnvironmentVariableGetAccessTokenViaRefreshTokenFunctionName = "USER_SERVICE_GET_ACCESS_TOKEN_VIA_REFRESH_TOKEN_FUNCTION_NAME" // #nosec G101
+	EnvironmentVariableUpdateUserByIdFunctionName                = "USER_SERVICE_UPDATE_USER_BY_ID_FUNCTION_NAME"                  // #nosec G101
 
 	EnvironmentVariableJwtPrivateKey = "JWT_PRIVATE_KEY" // #nosec G101
 	EnvironmentVariableJwtPublicKey  = "JWT_PUBLIC_KEY"  // #nosec G101
